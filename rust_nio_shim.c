77940d04f5b30abce5adc7fc8544c443d9b58b9d
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2015-04-05 22:09:25 -0400

    Add explanation to header in rust_nio_shim.c

--- rust_nio_shim.c
+++ rust_nio_shim.c
@@ -1,6 +1,13 @@
 #include <nspireio/nspireio.h>
 #include <stdlib.h>
 
+/**
+ * If I knew how to cleanly implement these in rust, I would implement them in rust.
+ * But I don't, and so they're here to stay until someone submits a pull request.
+ *
+ * Thanks in advance.
+ */
+
 nio_console* alloc_console() {
   return malloc(sizeof(nio_console));
 }
